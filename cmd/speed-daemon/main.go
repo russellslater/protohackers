279912ceb8e060af66b113c748757de5d244dfc5
Russell Slater <russellslater@gmail.com>
2022-11-07 08:12:46 -0500

    Read IAmDispatcher and WantHeartbeat messages

--- cmd/speed-daemon/main.go
+++ cmd/speed-daemon/main.go
@@ -36,10 +36,21 @@ const (
 type msgType uint8
 
 type client struct {
-	addr   string
-	conn   net.Conn
-	reader *bufio.Reader
-	camera *camera
+	addr       string
+	conn       net.Conn
+	reader     *bufio.Reader
+	camera     *camera
+	dispatcher *dispatcher
+}
+
+type camera struct {
+	road  uint16
+	mile  uint16
+	limit uint16
+}
+
+type dispatcher struct {
+	roads []uint16
 }
 
 func (c *client) readMsg() msgType {
@@ -55,6 +66,20 @@ func (c *client) readUint16() uint16 {
 	// TODO: handle errors
 	binary.Read(c.reader, binary.BigEndian, &num)
 	return num
+}
+
+func (c *client) readUint16Array() []uint16 {
+	count, err := c.reader.ReadByte()
+	if err != nil {
+		return []uint16{}
+	}
+
+	arr := make([]uint16, uint8(count))
+	for i := 0; i < len(arr); i++ {
+		arr[i] = c.readUint16()
+	}
+
+	return arr
 }
 
 func (c *client) readUint32() uint32 {
@@ -77,12 +102,6 @@ func (c *client) readStr() string {
 	}
 
 	return string(buf)
-}
-
-type camera struct {
-	road  uint16
-	mile  uint16
-	limit uint16
 }
 
 func NewTicketServer(port int) *TicketServer {
@@ -163,14 +182,24 @@ func (s *TicketServer) serve(client *client) error {
 	for {
 		switch client.readMsg() {
 		case iAmCameraMsg:
-			// echo -e '\x80\x00\x7b\x00\x08\x00\x3c' > out
+			// echo -e -n '\x80\x00\x7b\x00\x08\x00\x3c' > out
 			log.Println("IAmCamera")
 			client.camera = &camera{
 				road:  client.readUint16(),
 				mile:  client.readUint16(),
 				limit: client.readUint16(),
 			}
+
 			log.Println("Camera: ", client.camera)
+		case iAmDispatcherMsg:
+			// echo -e -n '\x81\x03\x00\x42\x01\x70\x13\x88' > out
+			log.Println("IAmDispatcher")
+
+			client.dispatcher = &dispatcher{
+				roads: client.readUint16Array(),
+			}
+
+			log.Println("Dispatcher: ", client.dispatcher)
 		case plateMsg:
 			// echo -e '\x20\x04\x55\x4e\x31\x58\x00\x00\x03\xe8' > out
 			log.Println("Plate")
@@ -180,6 +209,13 @@ func (s *TicketServer) serve(client *client) error {
 
 			log.Println("Plate: ", plate)
 			log.Println("Timestamp: ", timestamp)
+		case wantHeartbeatMsg:
+			// echo -e -n '\x40\x00\x00\x04\xdb' > out
+			log.Println("Want Heartbeat")
+
+			interval := client.readUint32()
+
+			log.Println("Interval: ", interval)
 		default:
 			log.Println("Unknown message")
 		}
